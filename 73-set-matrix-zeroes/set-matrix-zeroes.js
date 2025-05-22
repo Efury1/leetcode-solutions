6a6fed19e189e0c886489bff192523724309c82a
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2025-05-22 12:45:46 +0100

    Time: 2 ms (78.10%) | Memory: 58.8 MB (75.77%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,13 +3,18 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
+    // This creates an empty set to reack row indices (i) that contain at least one zero
     const zeroRows = new Set();
     const zeroCols = new Set();
     
     // We are looping through matrix and scanning the matrix
+    // i goes over each row
     for (let i = 0; i < matrix.length; i++) {
+        // j goes over each column
          for (let j = 0; j < matrix[0].length; j++) {
+            // Check whether cells hold a 0
             if (matrix[i][j] === 0) {
+                // ZeroRows ia a set, calling add (i) insets the row index i into the set
                 zeroRows.add(i);
                 zeroCols.add(j);
             }
@@ -19,7 +24,9 @@ var setZeroes = function(matrix) {
 
     // overwrite the 0s
     for (let i = 0; i < matrix.length; i++) {
+        //
         for (let j = 0; j < matrix[0].length; j++) {
+            // has checks whether the exact value is present in the set returning t or f
             if (zeroRows.has(i) || zeroCols.has(j)) {
                 matrix[i][j] = 0;
             }
